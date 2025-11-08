64c8115551acdee3b97e2fdd787fb041afdbad3a
PoonguzhaliBaskar <71202140+PoonguzhaliBaskar@users.noreply.github.com>
2025-11-08 15:12:03 -0800

    Week1

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[]args) {
+float fl=91.0f;
+String str="Google";
+boolean isBeta=false;
+int releaseYearoftype=2008;
+char shortcutKey='C';
+System.out.println("Version oftype float with a value of"+" "+fl);
+System.out.println("developer oftype string with a value of"+" "+str);
+System.out.println("isBeta oftype boolean with a value of"+" "+isBeta);
+System.out.println("releaseYear oftype int with a value of"+" "+releaseYearoftype);
+System.out.println("shortcutKey oftype char with a value of"+" "+shortcutKey);
+
+}}
